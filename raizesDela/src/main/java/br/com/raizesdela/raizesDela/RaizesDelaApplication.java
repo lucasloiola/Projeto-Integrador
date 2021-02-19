beff935f31ef3799015097266f7f1eb10e956994
Lucas <lucasloiolaalmeida@gmail.com>
2021-02-19 11:00:00 -0300

    criação do Spring Initializr

---
+++ raizesDela/src/main/java/br/com/raizesdela/raizesDela/RaizesDelaApplication.java
@@ -0,0 +1,13 @@
+package br.com.raizesdela.raizesDela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaizesDelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaizesDelaApplication.class, args);
+	}
+
+}
---
+++ raizesDela/src/test/java/br/com/raizesdela/raizesDela/RaizesDelaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raizesdela.raizesDela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizesDelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
